9bdc1c60c2cbfd8f64f09ea05b70a6ebf124c251
Arti chaudhary <chaudhary.aarti1998@gmail.com>
2024-11-18 13:53:34 +0530

    Initial commit

---
+++ lib/widgets/chat_list.dart
@@ -0,0 +1,28 @@
+import 'package:chat_app_flutter/info.dart';
+import 'package:chat_app_flutter/widgets/sender_message_card.dart';
+import 'package:flutter/material.dart';
+import 'my_message_card.dart';
+
+
+class ChatList extends StatelessWidget {
+  const ChatList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: messages.length,
+      itemBuilder: (context, index) {
+        if (messages[index]['isMe'] == true) {
+          return MyMessageCard(
+            message: messages[index]['text'].toString(),
+            date: messages[index]['time'].toString(),
+          );
+        }
+        return SenderMessageCard(
+          message: messages[index]['text'].toString(),
+          date: messages[index]['time'].toString(),
+        );
+      },
+    );
+  }
+}
