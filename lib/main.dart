fa9de25c03310cc1797e6411847ab2d2c8f9a40f
Arti chaudhary <chaudhary.aarti1998@gmail.com>
2024-11-18 15:40:46 +0530

    firebase done

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:chat_app_flutter/colors.dart';
+import 'package:chat_app_flutter/firebase_options.dart';
 import 'package:chat_app_flutter/screens/mobile_layout_screen.dart';
 import 'package:chat_app_flutter/screens/web_layout_screen.dart';
 import 'package:chat_app_flutter/utils/responsive_layout.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
